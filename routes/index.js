aabfcf9acad8553d4b55b876a83040cc034c913f
kenput3r <devolve.wtf@gmail.com>
2017-10-20 16:44:58 -0700

    scrape example implemented

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const models = require('../models');
+
 module.exports = (app) => {
     app.get('/', (req, res) => {
         res.render('index', {
@@ -12,4 +16,31 @@ module.exports = (app) => {
             isAbout: true
         });
     });
+
+    app.get('/scrape', (req, res) => {
+        axios.get('http://www.echojs.com/').then(response => {
+            let $ = cheerio.load(response.data);
+
+            $('article h2').each(function(i, element) {
+                let result = {};
+
+                result.title = $(this).children('a').text();
+                result.link = $(this).children('a').attr('href');
+
+                models.Article.create(result).then(data => {
+                    res.json(data);
+                }).catch(error => {
+                    res.json(error);
+                });
+            });
+        });
+    });
+
+    app.get('/articles', (req, res) => {
+        models.Article.find({}).then(data => {
+            res.json(data);
+        }).catch(error => {
+            res.json(error);
+        });
+    })
 }
